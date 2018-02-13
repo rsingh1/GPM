559f39e6d814f5a7c300e045ebcedc165f922c80
Raghvendra Singh <rsingh.nitrr@gmail.com>
2018-02-13 22:34:06 +0530

    Add files via upload

--- script/nodeSendEmail.js
+++ script/nodeSendEmail.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 exports.sendNodeEmail = function(emailBody)
 {
 var nodemailer = require('nodemailer');
@@ -13,11 +12,10 @@ var transporter = nodemailer.createTransport({
 
 var mailOptions = {
   from: 'rsingh.nitrr@gmail.com',
-  to:   'rpsingh39@hotmail.com',
+  to:   'raghuvendra.singh@nagarro.com',
   subject: 'Sending Email using Node.js',
-  text: emailBody
+  html: emailBody
 };
-
 transporter.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log('Error Sending in Email: '+error);
@@ -26,32 +24,3 @@ transporter.sendMail(mailOptions, function(error, info){
     }
   });
 };
-=======
-exports.sendNodeEmail = function(emailBody)
-{
-var nodemailer = require('nodemailer');
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'rsingh.nitrr@gmail.com',
-    pass: 'Shred@07'
-  }
-});
-
-var mailOptions = {
-  from: 'rsingh.nitrr@gmail.com',
-  to:   'rpsingh39@hotmail.com',
-  subject: 'Sending Email using Node.js',
-  text: emailBody
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log('Error Sending in Email: '+error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-};
->>>>>>> 4e419fe04f837c18195bbb89c14d7e25cff06b3c
--- script/routes.js
+++ script/routes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require("path");
+var qs = require('querystring');
+var emailSender = require('./nodeSendEmail');
 router.get('/', function(req, res){
   res.sendFile('Index.html', { root: '.' });
 });
@@ -29,5 +31,52 @@ router.get('/images/hireTruck.png', function(req, res, next) {
 router.get('/images/storage.png', function(req, res, next) {
         res.sendFile('storage.png', { root: 'images' });
 });
+router.get('/images/finallogo1.png', function(req, res, next) {
+        res.sendFile('finallogo1.png', { root: 'images' });
+});
+router.get('/images/logo.png', function(req, res, next) {
+        res.sendFile('logo.png', { root: 'images' });
+});
+router.post('/', function (req, res) {
+  console.log('Post request recieved!');
+  let un, up;
+  console.log('body: ' + JSON.stringify(req.body));
+  un = JSON.parse(JSON.stringify(req.body));
+  let userName, userMobile, userEmail, userLocationFrom, userLocationTo;
+  userName = un.usrName  || "";
+  userMobile= un.usrMobile  || "";
+  userEmail = un.usrEmail || "";
+  userLocationFrom = un.usrLocationFrom || "";
+  userLocationTo = un.usrLocationTo || "";
+  let msgHTMLBODY = "<table style="+"'border: 1px solid #990000; border-collapse: collapse; background-color: #f1f1c1; width: 100%'"+
+  "<tr>"+
+    "<th>Form Item</th>"+
+    "<th>Value</th>"+
+  "</tr>"+
+  "<tr>"+
+  "<td>Customer Name</td>"+
+  "<td>"+userName+"</td>"  +
+  "</tr>"+
+  "<tr>"+
+  "<td>Mobile</td>"+
+  "<td>"+userMobile+"</td>"  +
+  "</tr>"+
+  "<tr>"+
+  "<td>Email</td>"+
+  "<td>"+userEmail+"</td>"  +
+  "<tr>"+
+  "<td>From Location</td>"+
+  "<td>"+userLocationFrom+"</td>"  +
+  "</tr>"+
+  "<tr>"+
+  "<td>To Location</td>" +
+  "<td>"+userLocationTo+"</td>"  +
+  "</tr>"+
+"</table>";
+//  let msgBody = "User Name: "+userName+"\n\nUser Mobile: "+userMobile+"\n\nUser Email:"+userEmail+"\n\nLocation From:  "+userLocationFrom+"\n\nLocationTo: "+userLocationTo;
+
+  emailSender.sendNodeEmail(msgHTMLBODY);
+  res.send({"Result" : "OK"});
+})
 
 module.exports = router;
